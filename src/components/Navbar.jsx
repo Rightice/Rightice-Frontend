38cac5bd1d3815132b5839960e063956d41135ac
Kije Williams <kijewilliams01@gmail.com>
2025-02-02 03:35:36 +0100

    Rightice

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,6 +60,11 @@ const Navbar = () => {
               </Link>
               <span className="absolute bottom-0 left-0 h-0.5 w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
             </li>
+            <li className="group relative inline-block border border-[#BA986B] rounded-[50px] pl-7 pr-7 pt-3 pb-3 hover:bg-[#BA986B] transition ease-in duration-300">
+              <Link to="/attorneys" onClick={() => setIsOpen(false)}>
+                Book Consultation
+              </Link>
+            </li>
           </ul>
         </div>
       </nav>
